b281aa8420e6b22ff9ac8a7bdbed44a594e6da88
Angelo Di Paolo <angelod101@gmail.com>
2016-11-03 20:13:00 -0400

    update to Swift 3 syntax

--- ELKeychain/AccessControlProtectionPolicy.swift
+++ ELKeychain/AccessControlProtectionPolicy.swift
@@ -21,13 +21,13 @@ public enum AccessControlProtectionPolicy {
 extension AccessControlProtectionPolicy: RawRepresentable {
     public init?(rawValue: String) {
         switch rawValue {
-        case String(kSecAttrAccessibleWhenUnlocked):                    self = whenUnlocked
-        case String(kSecAttrAccessibleAfterFirstUnlock):                self = afterFirstUnlock
-        case String(kSecAttrAccessibleAlways):                          self = always
-        case String(kSecAttrAccessibleWhenPasscodeSetThisDeviceOnly):   self = whenPasscodeSetThisDeviceOnly
-        case String(kSecAttrAccessibleWhenUnlockedThisDeviceOnly):      self = whenUnlockedThisDeviceOnly
-        case String(kSecAttrAccessibleAfterFirstUnlockThisDeviceOnly):  self = afterFirstUnlockThisDeviceOnly
-        case String(kSecAttrAccessibleAlwaysThisDeviceOnly):            self = alwaysThisDeviceOnly
+        case String(kSecAttrAccessibleWhenUnlocked):                    self = .whenUnlocked
+        case String(kSecAttrAccessibleAfterFirstUnlock):                self = .afterFirstUnlock
+        case String(kSecAttrAccessibleAlways):                          self = .always
+        case String(kSecAttrAccessibleWhenPasscodeSetThisDeviceOnly):   self = .whenPasscodeSetThisDeviceOnly
+        case String(kSecAttrAccessibleWhenUnlockedThisDeviceOnly):      self = .whenUnlockedThisDeviceOnly
+        case String(kSecAttrAccessibleAfterFirstUnlockThisDeviceOnly):  self = .afterFirstUnlockThisDeviceOnly
+        case String(kSecAttrAccessibleAlwaysThisDeviceOnly):            self = .alwaysThisDeviceOnly
         default: return nil
         }
         
@@ -35,13 +35,13 @@ extension AccessControlProtectionPolicy: RawRepresentable {
     
     public var rawValue: String {
         switch self {
-        case whenUnlocked:                      return String(kSecAttrAccessibleWhenUnlocked)
-        case afterFirstUnlock:                  return String(kSecAttrAccessibleAfterFirstUnlock)
-        case always:                            return String(kSecAttrAccessibleAlways)
-        case whenPasscodeSetThisDeviceOnly:     return String(kSecAttrAccessibleWhenPasscodeSetThisDeviceOnly)
-        case whenUnlockedThisDeviceOnly:        return String(kSecAttrAccessibleWhenUnlockedThisDeviceOnly)
-        case afterFirstUnlockThisDeviceOnly:    return String(kSecAttrAccessibleAfterFirstUnlockThisDeviceOnly)
-        case alwaysThisDeviceOnly:              return String(kSecAttrAccessibleAlwaysThisDeviceOnly)
+        case .whenUnlocked:                      return String(kSecAttrAccessibleWhenUnlocked)
+        case .afterFirstUnlock:                  return String(kSecAttrAccessibleAfterFirstUnlock)
+        case .always:                            return String(kSecAttrAccessibleAlways)
+        case .whenPasscodeSetThisDeviceOnly:     return String(kSecAttrAccessibleWhenPasscodeSetThisDeviceOnly)
+        case .whenUnlockedThisDeviceOnly:        return String(kSecAttrAccessibleWhenUnlockedThisDeviceOnly)
+        case .afterFirstUnlockThisDeviceOnly:    return String(kSecAttrAccessibleAfterFirstUnlockThisDeviceOnly)
+        case .alwaysThisDeviceOnly:              return String(kSecAttrAccessibleAlwaysThisDeviceOnly)
         }
     }
 }
