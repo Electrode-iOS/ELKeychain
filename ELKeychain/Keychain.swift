a641cc0b6c7aca436e43d3d25b235fd6d93ad6f1
Angelo Di Paolo <angelod101@gmail.com>
2019-10-08 07:19:41 -0700

    add full support for keychain access groups

--- ELKeychain/Keychain.swift
+++ ELKeychain/Keychain.swift
@@ -16,24 +16,24 @@ public final class Keychain {
         self.service = service
     }
     
-    public func set(_ data: Data, account: String, accessControl: AccessControlConvertible? = nil) throws {
-        try Keychain.set(data, account: account, service: service)
+    public func set(_ data: Data, account: String, accessGroup: String? = nil, accessControl: AccessControlConvertible? = nil) throws {
+        try Keychain.set(data, account: account, service: service, accessGroup: accessGroup)
     }
     
-    public func set(_ value: String, account: String, accessControl: AccessControlConvertible? = nil) throws {
-        try Keychain.set(value, account: account, service: service, accessControl: accessControl)
+    public func set(_ value: String, account: String, accessGroup: String? = nil, accessControl: AccessControlConvertible? = nil) throws {
+        try Keychain.set(value, account: account, service: service, accessGroup: accessGroup, accessControl: accessControl)
     }
     
-    public func get(account: String) throws -> Data? {
-        return try Keychain.get(account: account, service: service)
+    public func get(account: String, accessGroup: String? = nil) throws -> Data? {
+        return try Keychain.get(account: account, service: service, accessGroup: accessGroup)
     }
     
-    public func get(account: String) throws -> String? {
-        return try Keychain.get(account: account, service: service)
+    public func get(account: String, accessGroup: String? = nil) throws -> String? {
+        return try Keychain.get(account: account, service: service, accessGroup: accessGroup)
     }
     
-    public func delete(account: String) throws {
-        try Keychain.delete(account: account, service: service)
+    public func delete(account: String, accessGroup: String? = nil) throws {
+        try Keychain.delete(account: account, service: service, accessGroup: accessGroup)
     }
 }
 
@@ -165,12 +165,12 @@ extension Keychain {
      - parameter service: The service associated with the password item.
      - returns: Returns true if the password was stored successfully.
     */
-    public static func set(_ value: String, account: String, service: String, accessControl: AccessControlConvertible? = nil) throws {
+    public static func set(_ value: String, account: String, service: String, accessGroup: String? = nil, accessControl: AccessControlConvertible? = nil) throws {
         guard let data = value.data(using: String.Encoding.utf8) else {
             throw KeychainError.failedToEncodeStringAsData
         }
         
-        try self.set(data, account: account, service: service, accessControl: accessControl)
+        try self.set(data, account: account, service: service, accessGroup: accessGroup, accessControl: accessControl)
     }
     
     /**
@@ -180,8 +180,8 @@ extension Keychain {
      - parameter service: The service associated with the password item.
      - returns: The generic password string if found. Returns nil when the password cannot be found.
     */
-    public static func get(account: String, service: String) throws -> String? {
-        guard let data: Data = try self.get(account: account, service: service) else {
+    public static func get(account: String, service: String, accessGroup: String? = nil) throws -> String? {
+        guard let data: Data = try self.get(account: account, service: service, accessGroup: accessGroup) else {
             return nil
         }
         
